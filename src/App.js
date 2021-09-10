d4e03185b38e6167f8ef79c4fb99b499a8e12ac3
Abdullah Al Masud <masudctg00@gmail.com>
2021-09-11 00:05:15 +0600

    component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import "./App.css";
-
 import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <NavBar />
+        <News />
+      </div>
+    );
   }
 }
