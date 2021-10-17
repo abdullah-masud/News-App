48929d343e68c7d772499f6a68dc19e9053a18c7
Abdullah Al Masud <masudctg00@gmail.com>
2021-10-17 20:58:32 +0600

    Loading Spinner Added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   // constructor
@@ -13,13 +14,14 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4e7604820ba748d8917f30940b2315a3&page=1&pagesize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4e7604820ba748d8917f30940b2315a3&page=1&pagesize=${this.props.pagesize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
@@ -27,26 +29,33 @@ export class News extends Component {
     console.log("previous");
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4e7604820ba748d8917f30940b2315a3&page=${
       this.state.page - 1
-    }&pagesize=20`;
+    }&pagesize=${this.props.pagesize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false,
     });
   };
 
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pagesize)
+    ) {
     } else {
       let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4e7604820ba748d8917f30940b2315a3&page=${
         this.state.page + 1
-      }&pagesize=20`;
+      }&pagesize=${this.props.pagesize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -54,26 +63,30 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1>Daily News</h1>
+        <h1 className="text-center">Daily News</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4 col-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 65) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 65) : ""
-                  }
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://image.cnbcfm.com/api/v1/image/106922527-1628177099813-Weber-OB-210805-PRESS-11.jpg?v=1628177178"
-                  }
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4 col-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 65) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 65)
+                        : ""
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://image.cnbcfm.com/api/v1/image/106922527-1628177099813-Weber-OB-210805-PRESS-11.jpg?v=1628177178"
+                    }
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -87,6 +100,10 @@ export class News extends Component {
           <button
             type="button"
             className="btn btn-dark"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pagesize)
+            }
             onClick={this.handleNextClick}
           >
             Next &rarr;
